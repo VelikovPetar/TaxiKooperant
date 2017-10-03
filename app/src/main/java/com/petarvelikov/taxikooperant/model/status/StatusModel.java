5795194a4930abe0f84480dbc47d1caef20dc5a5
VelikovPetar <petarvelikov95@gmail.com>
2017-10-03 22:47:06 +0200

    Removed unnecessary data field.

--- app/src/main/java/com/petarvelikov/taxikooperant/model/status/StatusModel.java
+++ app/src/main/java/com/petarvelikov/taxikooperant/model/status/StatusModel.java
@@ -10,22 +10,14 @@ public class StatusModel {
     public static final int NETWORK = 1;
     public static final int GPS = 2;
 
-    public static final String NO_LOGGED_DRIVER = "Нема најавен возач";
-
-    private String driverStatus;
     private int networkStatus;
     private int locationServiceStatus;
     private int serverStatus;
 
-    public StatusModel(String driverStatus, int networkStatus, int locationServiceStatus, int serverStatus) {
-        this.driverStatus = driverStatus;
+    public StatusModel(int networkStatus, int locationServiceStatus, int serverStatus) {
         this.networkStatus = networkStatus;
         this.locationServiceStatus = locationServiceStatus;
         this.serverStatus = serverStatus;
-    }
-
-    public String getDriverStatus() {
-        return driverStatus;
     }
 
     public int getNetworkStatus() {
@@ -40,10 +32,6 @@ public class StatusModel {
         return serverStatus;
     }
 
-    public void setDriverStatus(String driverStatus) {
-        this.driverStatus = driverStatus;
-    }
-
     public void setNetworkStatus(int networkStatus) {
         this.networkStatus = networkStatus;
     }
@@ -56,11 +44,4 @@ public class StatusModel {
         this.serverStatus = serverStatus;
     }
 
-    @Override
-    public String toString() {
-        String net = networkStatus == NOT_CONNECTED ? "Not connected" : networkStatus == CONNECTING ? "Connecting" : "Connected";
-        String server = serverStatus == NOT_CONNECTED ? "Not connected" : serverStatus == CONNECTING ? "Connecting" : "Connected";
-        String loc = locationServiceStatus == NO_LOCATION_SERVICE ? "No service" : locationServiceStatus == NETWORK ? "Network" : "GPS";
-        return String.format("Network: %s\nServer: %s\nLocation: %s\n", net, server, loc);
-    }
 }
